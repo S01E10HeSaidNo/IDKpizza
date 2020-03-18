e4a2d8f7c660e39c546c2cef0cfa641d2d8eef8c
S01E10HeSaidNo <shift307@gmail.com>
2020-03-18 17:58:59 -0400

    test1

---
+++ randomshit.py
@@ -0,0 +1,24 @@
+#uhmmm idk idk
+
+class InventoryHoldings:
+    __init__(self):
+        self.stack = ()
+        
+def wentToDTAT(self):
+    self.stack = dtatHolding
+    if partHoldTime == #put time here :
+        partHoldTime == 'true'
+        if partHoldTime != #put time here :
+            partHoldTime != 'false'
+            
+            
+            while True partHoldTime == 'True' or 'False':
+                dtatHolding.append(nameOfPart)
+                elif:
+                    dtatHolding.pop(nameOfPart)
+
+def ifDtat():
+    inputFileName = input( "Enter O2D excel file name file name: ")
+    for line in inputFileName:
+        inputFileName = open("inputFileName", "r")
+        inputFileName.split(",")
